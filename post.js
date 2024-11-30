68b416d33ed86a8359b654edde8d62de8ec960d4
CliveLewis <clivethe14@gmail.com>
2024-11-29 22:57:27 -0500

    All post routes in working condition

--- post.js
+++ post.js
@@ -5,34 +5,42 @@ const express = require('express');
 const app = express();
 app.use(express.static("public"));
 const PORT = 8000;
+
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
 app.listen(PORT, () => {
     console.log('Server listening on port' + PORT);
 });
 
 const clientConfig = {
     user: 'postgres',
-    password: 'mypacepokedexpostgresql',
-    host: 'pokedex-rds.cfqagimcm2pp.us-east-2.rds.amazonaws.com',
+    password: 'mypacepostgresql',
+    host: 'my-pace-postgresql.cb8ea6cg2ykd.us-east-2.rds.amazonaws.com',
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
     }
-
 };
 
 app.post("/pokemon", async (req, res) => {
     try {
-        const pokemon = JSON.parse(req.query['details']);
+        const pokemon = JSON.parse(req.body["details"])["pokemons"][0];
         const stats = pokemon["stats"];
         const moves = pokemon["moves"];
+        const types = pokemon["type"];
         const client = new Client(clientConfig);
         await client.connect();
-        const pokemon_query = await client.query("INSERT INTO POKEMON(name,height,weight,species_id) VALUES ($1::text,$2::text,$3::text,$4::integer) RETURNING *;", [pokemon['pokemon_name'], pokemon['height'], pokemon['weight'], pokemon['species_id']]);
-        pokemon_row = pokemon_query["rows"][0];
+        const pokemon_query = await client.query("INSERT INTO POKEMON(name,height,weight,species_id) VALUES ($1::text,$2::integer,$3::integer,$4::integer) RETURNING *;", [pokemon['pokemon_name'], parseInt(pokemon['height']), parseInt(pokemon['weight']), parseInt(pokemon['species_id'])]);
+        let pokemon_row = pokemon_query["rows"][0];
         const pokemon_base_stats_query = await client.query("INSERT INTO POKEMON_BASE_STATS(pokemon_id,hp,attack,defense,special_attack,special_defense,speed) VALUES ($1::smallint,$2::smallint,$3::smallint,$4::smallint,$5::smallint,$6::smallint,$7::smallint);", [pokemon_row["id"], stats['hp'], stats['attack'], stats['defense'], stats['special_attack'], stats['special_defense'], stats['speed']]);
-        moves.forEach(async (id) => {
-            let pokemon_moves_query = await client.query("INSERT INTO POKEMON_MOVES(pokemon_id,move_id) VALUES ($1::integer);", [pokemon_row["id"], id]);
+        await moves.forEach(async (id) => {
+            let pokemon_moves_query = await client.query("INSERT INTO POKEMON_MOVES(pokemon_id,move_id) VALUES ($1::integer,$2::integer);", [parseInt(pokemon_row["id"]), parseInt(id)]);
         });
+        types.forEach(async (id) => {
+            let pokemon_types_query = await client.query("INSERT INTO POKEMON_TYPES(pokemon_id,type_id) VALUES ($1::integer,$2::integer)", [pokemon_row["id"], id]);
+        });
+
         res.set("Content-Type", "application/json");
         res.send("Pokemon added successfully!");
     } catch (ex) {
