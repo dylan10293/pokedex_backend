c86038f5b4a7bdee3cffdb95f8cd2cf312014e41
Jia Lau <jia@jias-air.mynetworksettings.com>
2024-11-29 19:48:33 -0500

    Updated all get functions/queries

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 const clientConfig = {
     user: 'postgres',
     password: 'mypacepostgresql',
-    host: 'my-pace-postgresql.cb8ea6cg2ykd.us-east-2.rds.amazonaws.com',
+    host: 'my-pace-postgresql1.cbwycgms0wa6.us-east-2.rds.amazonaws.com',
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
@@ -33,88 +33,121 @@ app.get('/country', async function (req, res) {
     }
     await client.end();
 });
-
 // List of all pokemons with all details
-app.get('/pokemon', async function(res){ 
+app.get('/pokemon', async function (req, res) {
     try {
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON");
+        const query = `
+            SELECT 
+                p.id, 
+                p.name, 
+                m.name AS moves, 
+                t.name AS type, 
+                pb.hp, 
+                pb.attack, 
+                pb.defense, 
+                pb.special_attack, 
+                pb.special_defense, 
+                pb.speed 
+            FROM pokemon p 
+            JOIN pokemon_moves pm ON p.id = pm.pokemon_id 
+            JOIN moves m ON pm.move_id = m.id 
+            JOIN pokemon_types pt ON p.id = pt.pokemon_id 
+            JOIN types t ON t.id = pt.type_Id 
+            JOIN pokemon_base_stats pb ON p.id = pb.pokemon_id
+        `;
+        const result = await client.query(query);
 
         res.set("Content-Type", "application/json");
         res.send(result.rows);
-    }
-    catch(ex){
+    } catch (ex) {
         res.status(500).send("Internal Error - No Pokemon Found");
     }
-})
+});
 
 // List of all details of that specific pokemon ID
-app.get('/pokemon/:id', async function(req,res){
+app.get('/pokemon/:id', async function (req, res) {
     try {
-        const { id } = req.params;
-        const client = new Client(clientConfig);
-        await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE ID=$1::text", [id]);
-        res.set("Content-Type", "application/json");
-        res.send(result.rows);
-    }
-    catch(e){
-        res.status(500).send(e.Message);
-    }
-})
+        const { id } = req.params; 
 
-// List of all the species and the species ID
-app.get('/pokemon/species', async function(req,res){
-    try {
-        const { species } = req.query;
         const client = new Client(clientConfig);
-        await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE species=$1::text", [species]);
-        res.set("Content-Type", "application/json");
-        res.send(result.rows);
-    }
-    catch(e){
-        res.status(500).send(e.Message);
+        await client.connect(); 
+
+        const query = `
+            SELECT 
+                p.id, 
+                p.name, 
+                m.name AS moves, 
+                t.name AS type, 
+                pb.hp, 
+                pb.attack, 
+                pb.defense, 
+                pb.special_attack, 
+                pb.special_defense, 
+                pb.speed 
+            FROM pokemon p 
+            JOIN pokemon_moves pm ON p.id = pm.pokemon_id 
+            JOIN moves m ON pm.move_id = m.id 
+            JOIN pokemon_types pt ON p.id = pt.pokemon_id 
+            JOIN types t ON t.id = pt.type_Id 
+            JOIN pokemon_base_stats pb ON p.id = pb.pokemon_id 
+            WHERE p.id = $1
+        `;
+        const result = await client.query(query, [id]);
+        res.json(result.rows);
+    } catch (e) {
+        res.status(500).send("Internal Server Error");
     }
 })
-// List of all details of that specific species ID
-app.get('/pokemon/species/:id', async function(req,res){
+// List of all the species and the species ID
+app.get('/species', async function(req,res){
     try {
-        const { species } = req.query;
+        const client = new Client(clientConfig);
+        await client.connect();
+        const result = await client.query("SELECT * FROM species");
+        res.set("Content-Type", "application/json");
+        res.send(result.rows);
+    } catch (e) {
+        res.status(500).send(e.message);
+    } 
+    })
+
+    // List of all details of that specific species ID
+app.get('/species/:id', async function(req,res){
+    try {
         const {id} = req.params;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE species=$1::text AND id = $1", [id,species]);
+        const result = await client.query("SELECT * FROM species WHERE id = $1", [id]);
         res.set("Content-Type", "application/json");
         res.send(result.rows);
     }
     catch(e){
-        res.status(500).send(e.Message);
+        res.status(500).send(e.message);
     }
 })
+
 // List of all moves in pokemon
-app.get('/pokemon/moves', async function(req,res){
+app.get('/moves', async function(req,res){
     try {
-        const { moves } = req.query;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE moves=$1::text", [moves]);
+        const result = await client.query("SELECT id,name FROM moves");
         res.set("Content-Type", "application/json");
         res.send(result.rows);
     }
     catch(e){
-        res.status(500).send(e.Message);
+        res.status(500).send(e.message);
     }
 })
 // List of all details of that specific move ID
-app.get('/pokemon/moves/:id', async function(req,res){
+app.get('/moves/:id', async function(req,res){
     try {
-        const { moves } = req.query;
         const {id} = req.params;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE moves=$1::text AND id = $1", [id,moves]);
+        const result = await client.query("SELECT id,name FROM moves WHERE id = $1", [id]);
         res.set("Content-Type", "application/json");
         res.send(result.rows);
     }
@@ -122,21 +155,27 @@ app.get('/pokemon/moves/:id', async function(req,res){
         res.status(500).send(e.Message);
     }
 })
+
 //List of all pokemon name, ID, and image(maybe) of that specific TYPE
-app.get('/pokemon/types/:type', async function(req,res){
+app.get('/pokemon/types/:type', async function(req, res) {
     try {
-        const { types } = req.query;
-        const {type} = req.params;
+        const { type } = req.params;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE types=$1::text AND type = $1", [types,type]);
+        const result = await client.query(`
+            SELECT t.name, p.name
+            FROM pokemon p
+            JOIN pokemon_types pt ON pt.pokemon_id = p.id
+            JOIN types t ON t.id = pt.type_Id
+            WHERE t.name = $1
+        `, [type]);
         res.set("Content-Type", "application/json");
         res.send(result.rows);
-    }
-    catch(e){
-        res.status(500).send(e.Message);
+    } catch (e) {
+        res.status(500).send(e.message);
     }
 })
+
 // List of all types in pokemon
 app.get('/types', async function(req,res){
     try {
@@ -147,47 +186,52 @@ app.get('/types', async function(req,res){
         res.send(result.rows);
     }
     catch(e){
-        res.status(500).send(e.Message);
+        res.status(500).send(e.message);
     }
 })
 // List of all details of that specific type ID
-app.get('/types/:id', async function(req,res){
+app.get('/types/:id', async function(req, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM POKEMON WHERE id = $1", [id]);
+        const result = await client.query(`
+            SELECT t.name, te.attacking_type_id, te.defending_type_id, te.effectiveness
+            FROM types t
+            JOIN type_effectiveness te
+                ON t.id = te.attacking_type_id
+                OR t.id = te.defending_type_id
+            WHERE t.id = $1
+        `, [id]);
         res.set("Content-Type", "application/json");
         res.send(result.rows);
-    }
-    catch(e){
-        res.status(500).send(e.Message);
+    } catch (e) {
+        res.status(500).send(e.message);
     }
 })
 // List of all natures in pokemon
-app.get('/nature', async function(req,res){
+app.get('/natures', async function(req,res){
     try {
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM nature");
+        const result = await client.query("SELECT name FROM natures");
         res.set("Content-Type", "application/json");
         res.send(result.rows);
     }
     catch(e){
-        res.status(500).send(e.Message);
+        res.status(500).send(e.message);
     }
 })
 // List of all details of that specific nature ID
-app.get('/nature/:id', async function(req,res){
+app.get('/natures/:id', async function(req, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const client = new Client(clientConfig);
         await client.connect();
-        const result = await client.query("SELECT * FROM nature WHERE id = $1", [id]);
+        const result = await client.query("SELECT * FROM natures WHERE id = $1", [id]);
         res.set("Content-Type", "application/json");
         res.send(result.rows);
+    } catch (e) {
+        res.status(500).send(e.message);
     }
-    catch(e){
-        res.status(500).send(e.Message);
-    }
-})
+});
